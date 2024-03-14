c27229f5c0b015b3a630f79987137bb9227e86ca
developertool45 <developertool45@gmail.com>
2024-03-14 17:34:41 +0530

    add files

---
+++ node/math.js
@@ -0,0 +1,20 @@
+const sum = (a, b) => a + b;
+const mul = (a, b) => a * b;
+const g = 9.8;
+const PI = 3.15;
+
+//  module.exports.obj = {
+//     sum,
+//     mul,
+//     g,
+//     PI
+// }
+
+exports.obj = {
+    sum,
+    mul,
+    g,
+    PI
+}
+
+// module.exports = obj;
